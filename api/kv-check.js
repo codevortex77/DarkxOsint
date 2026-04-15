08e8a2e19370f65fb947aa89954becca35e91751
codevortex77 <codevortex92@gmail.com>
2026-04-15 13:17:13 +0530

    Update kv-check.js

--- api/kv-check.js
+++ api/kv-check.js
@@ -1,13 +1,11 @@
-// api/kv-check.js
-// Tells the frontend whether Vercel KV is configured
-
-export default function handler(req, res) {
+module.exports = function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  
+  res.setHeader('Content-Type', 'application/json');
+
   const kvConfigured = !!(
-    process.env.KV_REST_API_URL && 
+    process.env.KV_REST_API_URL &&
     process.env.KV_REST_API_TOKEN
   );
 
   res.status(200).json({ kv: kvConfigured });
-}
+};
